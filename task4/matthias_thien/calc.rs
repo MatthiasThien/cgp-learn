afcf4295bd7c03d6a7dabf200deebe1bca6d4906
MatthiasThien <matthien@cip77.informatik.uni-osnabrueck.de>
2016-07-20 11:16:14 +0200

    calc.rs v2

--- task4/matthias_thien/calc.rs
+++ task4/matthias_thien/calc.rs
@@ -1,7 +1,7 @@
 use std::ops::{Mul, Add};
 
 fn calc<T: Mul + Add + Copy>(eins: T, zwei: T) -> (<T as Add>::Output, <T as Mul>::Output) {
-    ((eins + zwei), (eins * zwei))
+    (eins + zwei, eins * zwei)
 }
 
 fn main() {
